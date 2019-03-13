053f375d35489bacb852ca55c4fb10b9aab52986
Kyle <kyle.rozanitis@gmail.com>
2019-03-13 13:13:50 -0400

    initial express server with route files

---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+// Location, bio, experiences, education, social network links
+const express = require('express');
+const router = express.Router();
+
+// @route   GET to api/profile/test
+// @desc    Tests profile route
+// @access  Public route
+router.get('/test', (req, res) => res.json({ msg: 'Profile works.' }));
+
+module.exports = router;
