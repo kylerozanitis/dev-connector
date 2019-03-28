2980a13dbafdcf546e494eda66d9c47109c5193f
Kyle <kyle.rozanitis@gmail.com>
2019-03-28 15:52:39 -0400

    Add ability to generate JWT token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://kyle:admin36@ds123635.mlab.com:23635/devconnectorkr'
+  mongoURI: 'mongodb://kyle:admin36@ds123635.mlab.com:23635/devconnectorkr',
+  secretOrKey: 'my coffee is black'
 };
