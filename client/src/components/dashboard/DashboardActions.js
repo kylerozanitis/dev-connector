fba917f030ad0a12041fc650395ed09adbbf9555
Kyle <kyle.rozanitis@gmail.com>
2019-06-11 18:51:37 -0400

    Change HTML to JSX syntax

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -3,18 +3,16 @@ import { Link } from 'react-router-dom';
 
 const DashboardActions = () => {
   return (
-    <div>
-      <div class="dash-buttons">
-        <Link to="/edit-profile" class="btn btn-light">
-          <i class="fas fa-user-circle text-primary" /> Edit Profile
-        </Link>
-        <Link to="/add-experience" class="btn btn-light">
-          <i class="fab fa-black-tie text-primary" /> Add Experience
-        </Link>
-        <Link to="/add-education" class="btn btn-light">
-          <i class="fas fa-graduation-cap text-primary" /> Add Education
-        </Link>
-      </div>
+    <div className="dash-buttons">
+      <Link to="/edit-profile" class="btn btn-light">
+        <i className="fas fa-user-circle text-primary" /> Edit Profile
+      </Link>
+      <Link to="/add-experience" class="btn btn-light">
+        <i className="fab fa-black-tie text-primary" /> Add Experience
+      </Link>
+      <Link to="/add-education" class="btn btn-light">
+        <i className="fas fa-graduation-cap text-primary" /> Add Education
+      </Link>
     </div>
   );
 };
